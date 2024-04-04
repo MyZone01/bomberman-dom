dfa64ca600006ed14c02f1d45660c7411cddc6c8
serignmbaye <barsaliou1998@gmail.com>
2024-04-04 13:21:18 +0000

    feat: add Player and PlayerManager classes

---
+++ server/core/playerManager.js
@@ -0,0 +1,13 @@
+class PlayersManager {
+  constructor() {
+    this.players = [];
+  }
+
+  addPlayer(player) {
+    this.players.push(player);
+  }
+
+  getPlayerById(id) {
+    return this.players.find(player => player.id === id);
+  }
+}
