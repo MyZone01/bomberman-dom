000a9feb1f921dc4a3c312c980ebf7aeeffe98a6
serignmbaye <barsaliou1998@gmail.com>
2024-04-06 16:09:27 +0000

    feat: add numberOfLife property and isDeath method to Player class

--- shared/models/player.js
+++ shared/models/player.js
@@ -2,5 +2,10 @@ export default class Player {
   constructor(id, nickname) {
     this.id = id;
     this.nickname = nickname;
+    this.numberOfLife = 3
+  }
+
+  isDeath() {
+    return this.numberOfLife === 0
   }
 }
